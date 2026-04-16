05185cb7d766308ba617ebce76acef1015d22997
Maciej876 <maciek.falek4@gmail.com>
2026-04-16 22:29:58 +0200

    struktura projektu

--- app/src/main/java/com/example/projekt2/ui/MainActivity.java
+++ app/src/main/java/com/example/projekt2/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.projekt2;
+package com.example.projekt2.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.projekt2.R;
 
 public class MainActivity extends AppCompatActivity {
 
