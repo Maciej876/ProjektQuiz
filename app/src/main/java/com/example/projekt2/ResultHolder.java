455a0e18e85f456f09952685fd0aacaba0cdccce
Maciej876 <maciek.falek4@gmail.com>
2026-04-16 23:01:35 +0200

    Przechowywyanie danych

---
+++ app/src/main/java/com/example/projekt2/ResultHolder.java
@@ -0,0 +1,11 @@
+package com.example.projekt2;
+
+import com.example.projekt2.model.Question;
+import java.util.List;
+
+public class ResultHolder {
+
+    public static List<Question> questions;
+    public static List<Integer> answers;
+
+}
