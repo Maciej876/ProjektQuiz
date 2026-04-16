6813d29129a974d2df956232d916cbb1aa3089cc
Maciej876 <maciek.falek4@gmail.com>
2026-04-16 22:38:50 +0200

    Ekran startowy

--- app/src/main/java/com/example/projekt2/ui/MainActivity.java
+++ app/src/main/java/com/example/projekt2/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projekt2.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +15,24 @@ import com.example.projekt2.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nameInput;
+    Button startBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        nameInput = findViewById(R.id.nameInput);
+        startBtn = findViewById(R.id.startButton);
+
+        startBtn.setOnClickListener(v -> {
+            String name = nameInput.getText().toString();
+
+            Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+            intent.putExtra("NAME", name);
+
+            startActivity(intent);
         });
     }
 }
